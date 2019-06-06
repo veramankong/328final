ac5fe38d9cf5360eb4c9d9866ffdbfbacf712b1c
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-06-06 18:15:50 -0400

    Added the ajax for service estimate id

--- model/id.php
+++ model/id.php
@@ -5,4 +5,34 @@
  * Date: 6/6/2019
  * Time: 12:45 PM
  */
-echo"Hi";
+
+
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+$user = $_SERVER['USER'];
+//require "/home/$user/config.php";
+require "/home2/$user/config.php";
+
+//Connect to the DB
+try{
+    //instantiate a database object
+    $dbh = new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
+}
+catch (PDOException $e)
+{
+    echo $e->getMessage();
+    return;
+}
+
+    //Define the query
+    $sql = "SELECT MAX(customer_id) FROM customer";
+//Prepare the statement
+    $statement = $dbh->prepare($sql);
+
+//Execute
+    $statement->execute();
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+        echo ($result+1);
+
+
+
--- model/lookup.php
+++ model/lookup.php
@@ -5,6 +5,47 @@
  * Date: 6/1/2019
  * Time: 12:45 PM
  */
- print_r($_POST);
-//$petid = $_POST['pid'];
+// print_r($_POST);
+$id = $_POST['pid'];
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+$user = $_SERVER['USER'];
+//require "/home/$user/config.php";
+require "/home2/$user/config.php";
+
+//Connect to the DB
+try {
+    //instantiate a database object
+    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    return;
+}
+
+if (!empty($_POST)) {
+    if (is_numeric($id)) {
+        //Define the query
+        $sql = "SELECT* FROM customer WHERE customer_id = :custid";
+//bind the parameter
+        $custid = $_POST['pid'];
+        $statement = $dbh->prepare($sql);
+
+
+        $statement->bindParam(':custid', $custid, PDO::PARAM_INT);
+
+// Execute the statement
+        $statement->execute();
+
+//process the result
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+        echo $row['fname'] . ', ' . $row['lname'] . $row['email'] . ', ' . $row['type'] . $row['services'];
+    } else {
+        echo "Please Enter a Service Estimate ID.";
+    }
+}
+
+if (empty($_POST)) {
+    echo "Please Enter a Service Estimate ID.";
+}
 
